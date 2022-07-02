7406dbb86fab9afb3bdd9bb2dd86c746486ca496
Justin Hurley <jhurley@velocityz.com>
2022-07-02 17:38:25 -0400

    test: Ensure Husky pre-commit hook works

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit
           {' '}
