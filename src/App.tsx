e6955acd4671b22b388393a2ed9fb4de8413a253
jhurleyvelocityz <76760081+jhurleyvelocityz@users.noreply.github.com>
2022-07-27 19:25:44 -0400

    test change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header-text">
         <img
           src={logo}
           className="App-logo"
